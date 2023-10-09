1f86229f99999e22d5c6108989353b7f35281126
brunet nicolas <brunet.nicolas35@gmail.com>
2023-10-09 18:39:30 +0200

    modification de travaux.js mais toujours pas d'affichage

--- Backend/travaux.js
+++ Backend/travaux.js
@@ -1,33 +1,32 @@
-const reponse = await fetch('http://localhost:5678/api/works',{
-});
+const reponse = await fetch("http://localhost:5678/api/works");
 const works =  await reponse.json();
-
-
-
-/*lieu où seront placées les travaux*/
-const sectionGallery = document.querySelector(".gallery")
 
 for (let i = 0; i < works.length; i++) {
 
+    /*Defini qu'un travail est un élément de works*/
     const travail = works[i];
 
-/*création de la balise <figure> dédié à 1 travail*/
-const element = document.createElement("figure");
+    /*lieu où seront placées les travaux*/
+    const sectionGallery = document.querySelector(".gallery");
 
-/*création de l'image, du titre et de la catégorie d'un travail*/
 
-const imageElement = document.createElement("img");
-imageElement.src = travail.imageUrl;
-const titreElement = document.createElement("h1");
-titreElement.src = travail.title;
-const categorieElement = document.createElement("h2");
-categorieElement.src = travail.catergory;
+    /*création de la balise <figure> dédié à 1 travail*/
+    const element = document.createElement("figure");
 
-/*récupération des élements dans une fiche dans la gallery*/
+    /*création de l'image, du titre et de la catégorie d'un travail*/
 
-sectionGallery.appendChild(travail);
-element.appendChild(imageElement);
-element.appendChild(titreElement);
-element.appendChild(categorieElement);
+    const imageElement = document.createElement("img");
+    imageElement.src = travail.imageUrl;
+    const titreElement = document.createElement("h1");
+    titreElement.src = travail.title;
+    const categorieElement = document.createElement("h2");
+    categorieElement.src = travail.catergory;
+
+    /*récupération des élements dans une fiche dans la gallery*/
+
+    sectionGallery.appendChild(element);
+    element.appendChild(imageElement);
+    element.appendChild(titreElement);
+    element.appendChild(categorieElement);
 
 }
