ef891964b16346df1187ebd918f907059f8d7506
brunet nicolas <brunet.nicolas35@gmail.com>
2023-10-24 17:35:45 +0200

    ajout du code pour se connecter et pour récuperer le token

--- FrontEnd/assets/js/login.js
+++ FrontEnd/assets/js/login.js
@@ -1,21 +1,94 @@
-const submit = document.querySelector('.seConnecter');
+/*
+function envoiEtVerif() {
+    console.log("récupération des identifiants");
+
+    const email = document.querySelector('#userInput').value;
+    const password = document.querySelector('#MDPInput').value;
+
+    console.log("Identifiant :", email, "Mot de passe :", password);
+
+    const data = {
+        email: email,
+        password: password,
+    };
+    console.log("ce qu'il y a dans la const data", data)
+
+    const optionRequete = {
+        method: 'POST',
+        headers: {
+        'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    };
+    console.log("ce que contient la const optionRequete", optionRequete)
+
+    fetch("http://localhost:5678/api/users/login", optionRequete)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Échec de la requête.');
+            }
+            return response.json(); // Analyser la réponse JSON
+            })
+            .then(data => {
+            // Gérer la réponse du serveur, par exemple, afficher un message de connexion réussie
+            console.log('Connexion réussie', data);
+            })
+            .catch(error => {
+            // Gérer les erreurs, par exemple, afficher un message d'erreur
+            console.error('Erreur de connexion :', error);
+            });
+}
+*/
 
 function authentification() {
-    const donnees = {
-        param1: document.getelementbyclassname(".userInput"), /*au lieu de param, mettre comme dans l'API*/
-        param2: document.getelementbyclassname(".MDPInput");
-    };
-    const options = {
-        method: 'POST',
-        body: JSON.stringify( donnees )
-    };
+    const lancement = document.getElementById('seConnecter');
+    console.log("Affiche ce qu'est 'lancement' :", lancement);
+    lancement.addEventListener("click", function () {
+        console.log("Le bouton 'Se connecter' fonctionne");
+        console.log("récupération des identifiants");
 
-    fetch( 'http://localhost:5678/api/user/login', options )
-    .then( response => response.json() )
-    .then( response => {
-        console.log("connecté !!!")
-    } );
-  
+        const email = document.querySelector('#userInput').value;
+        const password = document.querySelector('#MDPInput').value;
+    
+        console.log("Identifiant :", email, "Mot de passe :", password);
+    
+        const data = {
+            email: email,
+            password: password,
+        };
+        console.log("ce qu'il y a dans la const data", data)
+    
+        const optionRequete = {
+            method: 'POST',
+            headers: {
+            'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        };
+        console.log("ce que contient la const optionRequete", optionRequete)
+    
+        fetch("http://localhost:5678/api/users/login", optionRequete)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Échec de la requête.');
+                }
+                return response.json(); // Analyser la réponse JSON
+            })
+            .then(data => {
+                if (data.success) {
+                    const authToken = data.token;
+                    localStorage.setItem('authToken', authToken);
+                    console.log('Connexion réussie');
+
+                } else {
+                    console.log('Connexion echouée');
+                }
+            })
+            .catch(error => {
+            // Gérer les erreurs, par exemple, afficher un message d'erreur
+            console.error('Erreur de connexion :', error);
+            });
+    });
 }
 
-submit.addEventListener("click", authentification);
+authentification();
