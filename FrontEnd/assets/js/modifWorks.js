fbea5d2234e3d08099163b09bed0dee719592041
brunet nicolas <brunet.nicolas35@gmail.com>
2023-10-26 15:38:18 +0200

    ajout de la modale avec tous les travaux puis ajout de la modale pour envoyer le formulaire

--- FrontEnd/assets/js/modifWorks.js
+++ FrontEnd/assets/js/modifWorks.js
@@ -1,52 +1,119 @@
-
-document.addEventListener("DOMContentLoaded", function() {
-    // Votre code JavaScript ici
-    console.log("La page est entièrement chargée.");
-
-    // Vous pouvez accéder aux éléments HTML ici en toute sécurité.
-});
-
 const token = sessionStorage.getItem('token');
-console.log("le fameux token", token, );
+console.log("le fameux token", token);
 
- if (sessionStorage) {
-    console.log("Il y a un token dans le localStorage", sessionStorage);
+function afficherTravauxModal(works) {
+    for (let i = 0; i < works.length; i++) {
+        const travail = works[i];
+        const galleryModif = document.querySelector("#galleryModif");
+        const element = document.createElement("figure");
+        const imageElement = document.createElement("img");
+        imageElement.classList.add("modal-image");
 
-    const modification = document.querySelector(".lienModif");
-    const div = document.createElement("div");
+        imageElement.src = travail.imageUrl;
+        galleryModif.appendChild(element);
+        element.appendChild(imageElement);
+    }
+}
 
-    const lien = document.createElement("a");
-    lien.setAttribute("href", "fenetreModification.html");
+function nettoyerTravauxModal() {
+    const galleryModif = document.querySelector("#galleryModif");
+    while (galleryModif.firstChild) {
+        galleryModif.removeChild(galleryModif.firstChild);
+    }
+}
 
-    // Crée un élément d'icône (par exemple, une icône de crayon)
-    const icone = document.createElement("i");
-    icone.classList.add("fas", "fa-pen-to-square");
+async function travauxModal() {
+    const reponse = await fetch("http://localhost:5678/api/works", { method: 'GET' });
+    const travaux = await reponse.json();
 
-    // Ajoute l'icône à l'élément <a>
-    lien.appendChild(icone);
+    nettoyerTravauxModal(); // Nettoyez la modal avant d'ajouter de nouveaux travaux
+    afficherTravauxModal(travaux);
+    formulaireAjout();
+}
 
-    // Ajoute le texte "Modifier" à l'élément <a>
-    lien.appendChild(document.createTextNode("Modifier"));
+// Créez un bouton "Ajouter une photo" dans votre HTML avec l'id "ajouterPhotoButton"
 
-    div.appendChild(lien);
-    modification.appendChild(div);
+function formulaireAjout() {
+    const ajouterPhotoButton = document.getElementById('ajouterPhotoButton');
+    const closeModalForm = document.getElementById('closeModalForm');
+    const modalb = document.getElementById('modalb');
+    
+    ajouterPhotoButton.addEventListener('click', () => {
+        modalb.style.display = 'flex'; // Affiche la fenêtre modale d'ajout de photo
+    });
 
-    lien.addEventListener("click", function(e) {
-        e.preventDefault(); // Empêche le lien de suivre le lien d'origine
-        const largeurFenetre = 400;
-        const hauteurFenetre = 400;
-        const gauche = (window.screen.width - largeurFenetre) / 2;
-        const haut = (window.screen.height - hauteurFenetre) / 2;
-        window.open(
-            lien.getAttribute("href"), 
-            "fenetreModification", 
-            `width=${largeurFenetre},height=${hauteurFenetre},left=${gauche},top=${haut},resizable=no`
-        );
+    closeModalForm.addEventListener('click', () => {
+        modalb.style.display = 'none';
+    })
+
+    window.addEventListener('click', (event) => {
+        if (event.target == modalb) {
+            modalb.style.display = 'none';
+        }
     });
 }
-else {
-    console.log("Il n'y a rien dans le localStorage")
+
+    
+function envoyerForm () {// Gestionnaire d'événements pour soumettre le formulaire
+    const photoForm = document.getElementById('photoForm');
+    photoForm.addEventListener('submit', async (event) => {
+        event.preventDefault(); // Empêche la soumission du formulaire (à personnaliser)
+    
+        // Traitez le téléchargement de la photo ici (par exemple, à l'aide de fetch)
+        // Une fois le téléchargement terminé, vous pouvez réinitialiser le formulaire.
+        const formData = new FormData(photoForm);
+        const response = await fetch("URL_du_serveur_pour_télécharger_la_photo", {
+            method: 'POST',
+            body: formData,
+        });
+        // Ajoutez ici la logique pour gérer la réponse du serveur après le téléchargement.
+    
+        photoForm.reset(); // Réinitialisez le formulaire
+    });
 }
 
 
 
+
+
+
+
+
+
+//code se met en route s'il y a un token
+
+
+
+
+if (token) {
+    console.log("Il y a un token dans le sessionStorage");
+
+    const openModalButton = document.getElementById('openModal');
+    const closeModalButton = document.getElementById('closeModal');
+    const modal = document.getElementById('modal');
+    
+    const filtre = document.querySelector(".filtre");
+    filtre.style.display = "none";
+
+    openModalButton.addEventListener('click', () => {
+        modal.style.display = 'flex';
+        travauxModal();
+    });
+
+    // Fermer la fenêtre modale et nettoyer les travaux précédemment affichés
+    closeModalButton.addEventListener('click', () => {
+        modal.style.display = 'none';
+        nettoyerTravauxModal();
+    });
+
+    // Fermer la fenêtre modale en cliquant en dehors de la boîte de dialogue
+    window.addEventListener('click', (event) => {
+        if (event.target == modal) {
+            modal.style.display = 'none';
+            nettoyerTravauxModal();
+        }
+    });
+} else {
+    console.log("Il n'y a rien dans le sessionStorage");
+    modal.style.display = "none";
+}
